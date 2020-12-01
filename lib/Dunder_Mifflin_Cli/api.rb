0527b55b573d8b5dbe4daae603fb34899343c16c
Breanna <breanna@breannawebster.com>
2020-12-01 11:52:52 -0500

    commit

--- lib/Dunder_Mifflin_Cli/api.rb
+++ lib/Dunder_Mifflin_Cli/api.rb
@@ -1,3 +1,7 @@
 class Api
-    
+   def self.get_data
+    response = RestClient.get("https://www.officeapi.dev/api/quotes/")
+    data = JSON.parse(response.body)
+    binding.pry
+   end
 end
--- lib/Dunder_Mifflin_Cli/cli.rb
+++ lib/Dunder_Mifflin_Cli/cli.rb
@@ -2,5 +2,6 @@ class Cli
 
     def start
         puts "Welcome to Dunder Mifflin Paper Company!!"
+        Api.get_data
     end
 end
